f19579ceba483b688456b75b955ee35a392d48fc
0xAckerMan <joelkores1@gmail.com>
2023-11-18 21:26:40 +0300

    Puppet script to increase max traffic nginx can handle

--- alx-system_engineering-devops/0-the_sky_is_the_limit_not.pp
+++ alx-system_engineering-devops/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,10 @@
 
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin'
-}
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Restart Nginx server
-
+# Restart Nginx
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
